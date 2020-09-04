45c0d9b678b7cd5a95b5532eea38f31af1d74bad
Gustavo Parreira <gustavotcparreira@gmail.com>
2020-09-04 12:54:41 +0100

    feat: Updated packages and work on supporting dismissing reviews

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-const token = core.getInput('repo-token');
-const requestEvent = core.getInput('event');
+type RequestEvent = 'APPROVE' | 'COMMENT' | 'DISMISS' | 'REQUEST_CHANGES';
+
+const requestEvent = core.getInput('event') as RequestEvent;
 const body = core.getInput('body');
 
-const octokit = new github.GitHub(token);
+if (!process.env.GITHUB_TOKEN) {
+  core.setFailed('You have not passed a GITHUB_TOKEN to this action');
+}
+
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN as string);
 
 if (
   (requestEvent === 'COMMENT' || requestEvent === 'REQUEST_CHANGES') &&
@@ -20,17 +25,52 @@ if (!pullRequest) {
   core.setFailed('This action is meant to be ran on pull requests');
 }
 
-octokit
-  .graphql(
-    `
-      mutation {
-        addPullRequestReview(input: {
-          pullRequestId: "${(<any>pullRequest)['node_id']}",
-          event: ${requestEvent},
-          body: "${body}"
-        }) {clientMutationId} }`,
-  )
-  .catch((err) => {
-    core.error(err);
-    core.setFailed(err.message);
-  });
+const pullNumber: number = (<any>pullRequest)['node_id'];
+
+if (requestEvent === 'DISMISS') {
+  octokit.pulls
+    .listReviews({
+      ...github.context.repo,
+      pull_number: pullNumber,
+    })
+    .then(({data}) => {
+      data.find(({user}) => {
+        console.log(user);
+        return true;
+      });
+    });
+  // octokit.pulls.dismissReview({
+  //   ...github.context.repo,
+  //   pull_number: (<any>pullRequest)['node_id'],
+  //   message: body,
+  //   review_id: octokit.pulls.
+  // });
+} else {
+  octokit.pulls
+    .createReview({
+      ...github.context.repo,
+      pull_number: pullNumber,
+      event: requestEvent,
+      body,
+    })
+    .catch((err) => {
+      core.error(err);
+      core.setFailed(err.message);
+    });
+}
+
+// octokit
+//   .graphql(
+//     `
+//       mutation {
+//         addPullRequestReview(input: {
+//           pullRequestId: "${(<any>pullRequest)['node_id']}",
+//           event: ${requestEvent},
+//           body: "${body}"
+//         }) {clientMutationId} }`,
+//   )
+//   // .then()
+//   .catch((err) => {
+//     core.error(err);
+//     core.setFailed(err.message);
+//   });
